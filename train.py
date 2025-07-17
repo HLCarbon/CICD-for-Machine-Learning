d432d66f86e8c82506d1066f4a898be111af3a41
Carbon6C <henrique.lima.un@gmail.com>
2025-07-17 17:48:53 +0100

    fixed train.py

--- train.py
+++ train.py
@@ -1,15 +1,18 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.model_selection import train_test_split
-
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.impute import SimpleImputer
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 
+import skops.io as sio
 
-drug_df = pd.read_csv('Data/drug200.csv')
-drug_df = drug_df.sample(frac = 1)
+drug_df = pd.read_csv("Data/drug200.csv")
+drug_df = drug_df.sample(frac=1)
 
 X = drug_df.drop("Drug", axis=1).values
 y = drug_df.Drug.values
@@ -18,28 +21,25 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=125
 )
 
-cat_col = [1,2,3]
-num_col = [0,4]
+cat_col = [1, 2, 3]
+num_col = [0, 4]
 
 transform = ColumnTransformer(
     [
-        ("encoder", OrdinalEncoder(),cat_col),
-        ("num_imputer", SimpleImputer(strategy = "median"), num_col),
-        ("num_scaler", StandardScaler(),num_col)
+        ("encoder", OrdinalEncoder(), cat_col),
+        ("num_imputer", SimpleImputer(strategy="median"), num_col),
+        ("num_scaler", StandardScaler(), num_col),
     ]
 )
 
 pipeline = Pipeline(
     [
         ("preprocessing", transform),
-        ("model", RandomForestClassifier(n_estimators = 100, random_state = 125))
-
+        ("model", RandomForestClassifier(n_estimators=100, random_state=125)),
     ]
 )
 
 pipeline.fit(X_train, y_train)
-
-from sklearn.metrics import accuracy_score, f1_score
 
 predictions = pipeline.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
@@ -50,16 +50,11 @@ print("Accuracy:", str(round(accuracy, 2) * 100) + "%", "F1:", round(f1, 2))
 with open("Results/metrics.txt", "w") as outfile:
     outfile.write(f"\nAccuracy = {accuracy.round(2)}, F1 Score = {f1.round(2)}.")
 
-import matplotlib.pyplot as plt
-from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
-
 cm = confusion_matrix(y_test, predictions, labels=pipeline.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipeline.classes_)
 disp.plot()
 plt.savefig("Results/model_results.png", dpi=120)
 
-import skops.io from sio
-
 sio.dump(pipeline, "Model/drug_pipeline.skops")
 
-model = sio.load("Model/drug_pipeline.skops", trusted = True)
+model = sio.load("Model/drug_pipeline.skops", trusted=True)
